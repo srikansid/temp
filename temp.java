05d975b2867b03da5e81217f947e870018283512
srikansid <112045224+srikansid@users.noreply.github.com>
2025-06-02 16:14:21 -0400

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,12 +1,29 @@
-okta:
-  audience: api://default
-  client:
-    id: 0oa2g3bhh6fgbr1lw1d7
-  issuer: https://myaccessxtl-dev.faa.gov/oauth2/default
-  logout: 
-    uri: https://dev-uas-trust.faa.gov
-  redirect:  
-    uri: https://dev-uas-trust.faa.gov/login/callback
-  register:
-    uri: https://myaccessreg-stage.faa.gov/register 
-    
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+public class PropertyConfig{
+	
+	
+	@Value("${okta.audience}")
+	String oktaAudience;
+
+	@Value("${okta.client.id}")
+	String oktaClientId;
+	
+	@Value("${okta.issuer}")
+	String oktaIssuer;
+	
+	@Value("${okta.logout.uri}")
+	String oktaLogoutUri;
+	
+	@Value("${okta.redirect.uri}")
+	String oktaRedirectUri;
+	
+	@Value("${okta.register.uri}")
+	String oktaRegisterUri;
+
+}
