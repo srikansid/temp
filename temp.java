7de5fc3367144a9557af63f232a90721974ae69f
srikansid <112045224+srikansid@users.noreply.github.com>
2025-06-02 16:09:07 -0400

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,60 +1,12 @@
-package gov.faa.uastrust.config;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class UasTrustContext {
-	
-	private static final ThreadLocal<Map<String, String>> CONTEXT = ThreadLocal.withInitial(HashMap::new);
-	private static final String USER_NAME = "UserName";
-	private static final String USER_ROLE = "UserRole";
-	private static final String USER_TYPE = "UserType";
-	private static final String USER_IP_ADDRESS = "UserIpAddress";
-	private static final String USER_HOST_NAME = "UserHostName";
-	
-	private static void setAttribute(String attributeName, String attributeValue) {
-		CONTEXT.get().put(attributeName, attributeValue);
-	}
-	
-	public static String getUserName() {
-		return CONTEXT.get().get(USER_NAME);
-	}
-
-	public static void setUserName(String userNameValue) {
-		setAttribute(USER_NAME, userNameValue);
-	}
-	
-	public static String getUserRole() {
-		return CONTEXT.get().get(USER_NAME);
-	}
-
-	public static void setUserRole(String userRoleValue) {
-		setAttribute(USER_ROLE, userRoleValue);
-	}
-	
-	public static String getUserType() {
-		return CONTEXT.get().get(USER_NAME);
-	}
-
-	public static void setUserType(String userTypeValue) {
-		setAttribute(USER_TYPE, userTypeValue);
-	}
-
-	public static String getUserIpAddress() {
-		return CONTEXT.get().get(USER_IP_ADDRESS);
-	}
-
-	public static void setUserIpAddress(String userIpAddressValue) {
-		setAttribute(USER_IP_ADDRESS, userIpAddressValue);
-	}
-
-	public static String getUserHostName() {
-		return CONTEXT.get().get(USER_HOST_NAME);
-	}
-
-	public static void setUserHostName(String userHostNameValue) {
-		setAttribute(USER_HOST_NAME, userHostNameValue);
-	}
-	
-
-}
+okta:
+  audience: api://default
+  client:
+    id: 0oa2g3bhh6fgbr1lw1d7
+  issuer: https://myaccessxtl-dev.faa.gov/oauth2/default
+  logout: 
+    uri: https://dev-uas-trust.faa.gov
+  redirect:  
+    uri: https://dev-uas-trust.faa.gov/login/callback
+  register:
+    uri: https://myaccessreg-stage.faa.gov/register 
+    
